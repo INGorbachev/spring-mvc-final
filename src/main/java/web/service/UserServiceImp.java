482f25acd06272305392bb6a5a593aa84c10d6f1
INGorbachev <81094561+INGorbachev@users.noreply.github.com>
2021-10-11 13:55:47 +0300

    Add files via upload

---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,42 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = false)
+public class UserServiceImp implements UserService {
+
+   @Autowired
+   private UserDao userDao;
+
+   @Override
+   public void add(User user) {
+      userDao.add(user);
+   }
+
+   @Override
+   public List<User> listUsers() {
+      return userDao.listUsers();
+   }
+
+   @Override
+   public void remove(Long id) {
+      userDao.remove(id);
+   }
+
+   @Override
+   public void update(User user) {
+      userDao.update(user);
+   }
+
+   @Override
+   public User getUserById(Long id) {
+      return userDao.getUserById(id);
+   }
+}
